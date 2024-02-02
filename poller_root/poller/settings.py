e3489eb32d34fb8d2f31fc775e54c041fe3be1b9
Алексей Коган (рабочий) <alekseikogan@yandex.ru>
2024-02-02 16:39:05 +0300

    add html, urls, views

--- poller_root/poller/settings.py
+++ poller_root/poller/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'poller.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'db.sqlite3'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
